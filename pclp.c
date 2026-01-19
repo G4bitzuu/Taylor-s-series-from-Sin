748342b829cf0994340bf8f3b8be485527e9bd19
G4bitzuu <padurarudumitru121@gmail.com>
2026-01-19 13:58:31 +0000

    Initial commit: Taylor series approximation

--- pclp.c
+++ pclp.c
@@ -1,50 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define _USE_MATH_DEFINES
 #include <math.h>
-#include <conio.h> 
-
-double sin_taylor(double x, int n_terms) {
-    double sum = 0.0;
-    double term = x;
-    
-    for (int i = 0; i < n_terms; i++) {
-        sum += term;
-        term = term * (-x * x) / ((2 * i + 2) * (2 * i + 3));
-    }
-    return sum;
-}
 
 int main() {
-    double angle, rad;
+    double f, x, error;
     char option;
 
     do {
-        printf("\n--- Sine Calculation (Taylor Series) ---");
-        printf("\nEnter the angle in degrees: ");
-        
-        if (scanf("%lf", &angle) != 1) {
-            printf("Invalid input! Exiting...\n");
-            exit(1);
-        }
-
-        rad = angle * (M_PI / 180.0);
-
-        printf("\n1. Taylor Series (10 terms): %.10f", sin_taylor(rad, 10));
-        printf("\n2. Standard math.h sin():     %.10f", sin(rad));
-        printf("\n3. Absolute Error:            %.10e\n", fabs(sin_taylor(rad, 10) - sin(rad)));
-
-        printf("\nWould you like to continue? (n/N to Exit, any other key to continue): ");
-        scanf(" %c", &option);
-
-        if (option == 'n' || option == 'N') {
+        printf("Enter number of terms (f) and exponent (x): ");
+        if (scanf("%lf %lf", &f, &x) != 2) {
+            printf("Error: Invalid input data!\n");
             break;
         }
 
-    } while (1);
+        double e_taylor = 1.0;
+        double current_term = 1.0;
+        int n_terms = (int)fabs(f);
 
-    printf("\nProgram finished. Press any key to close...");
-    getch();
+        for (int i = 1; i < n_terms; i++) {
+            current_term = current_term * x / i;
+            e_taylor += current_term;
+        }
+
+        error = fabs(e_taylor - exp(x));
+
+        printf("Taylor: %g | exp(x): %g\n", e_taylor, exp(x));
+        printf("Absolute Error: %g\n", error);
+
+        printf("Continue? (Y/N): ");
+        scanf(" %c", &option);
+
+    } while (option != 'n' && option != 'N');
+
     return 0;
 }
